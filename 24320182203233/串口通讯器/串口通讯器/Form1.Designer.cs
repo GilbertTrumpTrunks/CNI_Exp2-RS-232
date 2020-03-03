55a106ec3de43897ffb522de25c492c57e70adaa
GilbertTrumpTrunks <1191698255@qq.com>
2020-03-03 20:18:46 +0800

    lyc_lxy

---
+++ 24320182203233/串口通讯器/串口通讯器/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace 串口通讯器
+{
+    partial class 串口通讯器
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_BaudRate = new System.Windows.Forms.ComboBox();
+            this.comboBox_Port = new System.Windows.Forms.ComboBox();
+            this.comboBox_CheckBits = new System.Windows.Forms.ComboBox();
+            this.comboBox_StopBits = new System.Windows.Forms.ComboBox();
+            this.comboBox_DataBits = new System.Windows.Forms.ComboBox();
+            this.button_Switch = new System.Windows.Forms.Button();
+            this.textBox_Send = new System.Windows.Forms.TextBox();
+            this.textBox_Receive = new System.Windows.Forms.TextBox();
+            this.button_Send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox_BaudRate
+            // 
+            this.comboBox_BaudRate.FormattingEnabled = true;
+            this.comboBox_BaudRate.Location = new System.Drawing.Point(136, 27);
+            this.comboBox_BaudRate.Name = "comboBox_BaudRate";
+            this.comboBox_BaudRate.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_BaudRate.TabIndex = 0;
+            this.comboBox_BaudRate.Text = "波特率";
+            this.comboBox_BaudRate.SelectedIndexChanged += new System.EventHandler(this.baud_SelectedIndexChanged);
+            // 
+            // comboBox_Port
+            // 
+            this.comboBox_Port.FormattingEnabled = true;
+            this.comboBox_Port.Location = new System.Drawing.Point(12, 27);
+            this.comboBox_Port.Name = "comboBox_Port";
+            this.comboBox_Port.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_Port.TabIndex = 1;
+            this.comboBox_Port.Text = "串口";
+            // 
+            // comboBox_CheckBits
+            // 
+            this.comboBox_CheckBits.FormattingEnabled = true;
+            this.comboBox_CheckBits.Location = new System.Drawing.Point(263, 27);
+            this.comboBox_CheckBits.Name = "comboBox_CheckBits";
+            this.comboBox_CheckBits.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_CheckBits.TabIndex = 2;
+            this.comboBox_CheckBits.Text = "奇偶校验";
+            // 
+            // comboBox_StopBits
+            // 
+            this.comboBox_StopBits.FormattingEnabled = true;
+            this.comboBox_StopBits.Location = new System.Drawing.Point(517, 27);
+            this.comboBox_StopBits.Name = "comboBox_StopBits";
+            this.comboBox_StopBits.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_StopBits.TabIndex = 3;
+            this.comboBox_StopBits.Text = "停止位";
+            // 
+            // comboBox_DataBits
+            // 
+            this.comboBox_DataBits.FormattingEnabled = true;
+            this.comboBox_DataBits.Location = new System.Drawing.Point(390, 27);
+            this.comboBox_DataBits.Name = "comboBox_DataBits";
+            this.comboBox_DataBits.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_DataBits.TabIndex = 4;
+            this.comboBox_DataBits.Text = "数据位";
+            // 
+            // button_Switch
+            // 
+            this.button_Switch.Location = new System.Drawing.Point(644, 27);
+            this.button_Switch.Name = "button_Switch";
+            this.button_Switch.Size = new System.Drawing.Size(75, 23);
+            this.button_Switch.TabIndex = 5;
+            this.button_Switch.Text = "开启";
+            this.button_Switch.UseVisualStyleBackColor = true;
+            this.button_Switch.Click += new System.EventHandler(this.button_Switch_Click_1);
+            // 
+            // textBox_Send
+            // 
+            this.textBox_Send.Location = new System.Drawing.Point(12, 89);
+            this.textBox_Send.Multiline = true;
+            this.textBox_Send.Name = "textBox_Send";
+            this.textBox_Send.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Send.Size = new System.Drawing.Size(629, 150);
+            this.textBox_Send.TabIndex = 6;
+            // 
+            // textBox_Receive
+            // 
+            this.textBox_Receive.Location = new System.Drawing.Point(12, 279);
+            this.textBox_Receive.Multiline = true;
+            this.textBox_Receive.Name = "textBox_Receive";
+            this.textBox_Receive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Receive.Size = new System.Drawing.Size(629, 150);
+            this.textBox_Receive.TabIndex = 8;
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(647, 216);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(75, 23);
+            this.button_Send.TabIndex = 9;
+            this.button_Send.Text = "发送";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "串口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "波特率";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "奇偶校验";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "数据位";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(517, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "停止位";
+            // 
+            // 串口通讯器
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.textBox_Receive);
+            this.Controls.Add(this.textBox_Send);
+            this.Controls.Add(this.button_Switch);
+            this.Controls.Add(this.comboBox_DataBits);
+            this.Controls.Add(this.comboBox_StopBits);
+            this.Controls.Add(this.comboBox_CheckBits);
+            this.Controls.Add(this.comboBox_Port);
+            this.Controls.Add(this.comboBox_BaudRate);
+            this.Name = "串口通讯器";
+            this.Text = "串口通讯器";
+            this.Load += new System.EventHandler(this.串口通讯器_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_BaudRate;
+        private System.Windows.Forms.ComboBox comboBox_Port;
+        private System.Windows.Forms.ComboBox comboBox_CheckBits;
+        private System.Windows.Forms.ComboBox comboBox_StopBits;
+        private System.Windows.Forms.ComboBox comboBox_DataBits;
+        private System.Windows.Forms.Button button_Switch;
+        private System.Windows.Forms.TextBox textBox_Send;
+        private System.Windows.Forms.TextBox textBox_Receive;
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 24320182203233/串口通讯器/串口通讯器/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace 串口通讯器
+{
+    public partial class 串口通讯器 : Form
+    {
+
+        public SerialPort ComDevice = new SerialPort();
+
+        public 串口通讯器()
+        {
+            InitializeComponent();
+
+            InitralConfig();
+        }
+
+        private void InitralConfig()
+        {
+            comboBox_Port.Items.AddRange(SerialPort.GetPortNames());
+
+
+            if (comboBox_Port.Items.Count > 0)
+            {
+                comboBox_Port.SelectedIndex = 0;
+            }
+            else
+            {
+                comboBox_Port.Text = "未检测到串口";
+            }
+
+            //波特率
+            comboBox_BaudRate.Items.Add("110");
+            comboBox_BaudRate.Items.Add("300");
+            comboBox_BaudRate.Items.Add("1200");
+            comboBox_BaudRate.Items.Add("2400");
+            comboBox_BaudRate.Items.Add("4800");
+            comboBox_BaudRate.Items.Add("9600");
+            comboBox_BaudRate.SelectedIndex = 5;
+
+            //数据位
+            comboBox_DataBits.Items.Add("5");
+            comboBox_DataBits.Items.Add("6");
+            comboBox_DataBits.Items.Add("7");
+            comboBox_DataBits.Items.Add("8");
+            comboBox_DataBits.SelectedIndex = 3;
+
+            //停止位
+            comboBox_StopBits.Items.Add("1");
+            comboBox_StopBits.Items.Add("2");
+            comboBox_StopBits.SelectedIndex = 0;
+
+            //校验位
+            comboBox_CheckBits.Items.Add("无");
+            comboBox_CheckBits.SelectedIndex = 0;
+
+
+            //向ComDevice.DataReceived（是一个事件）注册一个方法Com_DataReceived，当端口类接收到信息时时会自动调用Com_DataReceived方法
+            ComDevice.DataReceived += new SerialDataReceivedEventHandler(Com_DataReceived);
+
+        }
+
+        private void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //开辟接收缓冲区
+            byte[] ReDatas = new byte[ComDevice.BytesToRead];
+            //从串口读取数据
+            ComDevice.Read(ReDatas, 0, ReDatas.Length);
+            //实现数据的解码与显示
+            AddData(ReDatas);
+        }
+
+        public void AddData(byte[] data)
+        {
+            AddContent(new ASCIIEncoding().GetString(data));
+        }
+
+        private void AddContent(string content)
+        {
+            BeginInvoke(new MethodInvoker(delegate
+            {
+                textBox_Receive.AppendText(content);
+            }));
+        }
+
+        private void button_Switch_Click(object sender, EventArgs e)
+        {
+            if (comboBox_Port.Items.Count <= 0)
+            {
+                MessageBox.Show("未发现可用串口，请检查硬件设备");
+                return;
+            }
+
+            if (ComDevice.IsOpen == false)
+            {
+                //设置串口相关属性
+                ComDevice.PortName = comboBox_Port.SelectedItem.ToString();
+                ComDevice.BaudRate = Convert.ToInt32(comboBox_BaudRate.SelectedItem.ToString());
+                ComDevice.Parity = (Parity)Convert.ToInt32(comboBox_CheckBits.SelectedIndex.ToString());
+                ComDevice.DataBits = Convert.ToInt32(comboBox_DataBits.SelectedItem.ToString());
+                ComDevice.StopBits = (StopBits)Convert.ToInt32(comboBox_StopBits.SelectedItem.ToString());
+                try
+                {
+                    //开启串口
+                    ComDevice.Open();
+                    button_Send.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "未能成功开启串口", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                button_Switch.Text = "关闭";
+                //pictureBox_Status.BackgroundImage = Properties.Resources.green;
+            }
+            else
+            {
+                try
+                {
+                    //关闭串口
+                    ComDevice.Close();
+                    button_Send.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "串口关闭错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                button_Switch.Text = "开启";
+                //pictureBox_Status.BackgroundImage = Properties.Resources.red;
+            }
+
+            comboBox_Port.Enabled = !ComDevice.IsOpen;
+            comboBox_BaudRate.Enabled = !ComDevice.IsOpen;
+            comboBox_DataBits.Enabled = !ComDevice.IsOpen;
+            comboBox_StopBits.Enabled = !ComDevice.IsOpen;
+            comboBox_CheckBits.Enabled = !ComDevice.IsOpen;
+        }
+
+        private void baud_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void button_Send_Click(object sender, EventArgs e)
+        //{
+        //    if (textBox_Receive.Text.Length > 0)
+        //    {
+        //        textBox_Receive.AppendText("\n");
+        //    }
+
+        //    byte[] sendData = null;
+
+        //    sendData = Encoding.ASCII.GetBytes(textBox_Send.Text.Trim());
+
+        //    SendData(sendData);
+        //}
+
+        public bool SendData(string data)
+        {
+            if (ComDevice.IsOpen)
+            {
+                try
+                {
+                    //将消息传递给串口
+                    ComDevice.Write(data);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "发送失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("串口未开启", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
+        }
+
+        private void 串口通讯器_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Switch_Click_1(object sender, EventArgs e)
+        {
+            if (comboBox_Port.Items.Count <= 0)
+            {
+                MessageBox.Show("未发现可用串口，请检查硬件设备");
+                return;
+            }
+
+            if (ComDevice.IsOpen == false)
+            {
+                //设置串口相关属性
+                ComDevice.PortName = comboBox_Port.SelectedItem.ToString();
+                ComDevice.BaudRate = Convert.ToInt32(comboBox_BaudRate.SelectedItem.ToString());
+                ComDevice.Parity = (Parity)Convert.ToInt32(comboBox_CheckBits.SelectedIndex.ToString());
+                ComDevice.DataBits = Convert.ToInt32(comboBox_DataBits.SelectedItem.ToString());
+                ComDevice.StopBits = (StopBits)Convert.ToInt32(comboBox_StopBits.SelectedItem.ToString());
+                try
+                {
+                    //开启串口
+                    ComDevice.Open();
+                    button_Send.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "未能成功开启串口", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                button_Switch.Text = "关闭";
+                //pictureBox_Status.BackgroundImage = Properties.Resources.green;
+            }
+            else
+            {
+                try
+                {
+                    //关闭串口
+                    ComDevice.Close();
+                    button_Send.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "串口关闭错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                button_Switch.Text = "开启";
+                //pictureBox_Status.BackgroundImage = Properties.Resources.red;
+            }
+
+            comboBox_Port.Enabled = !ComDevice.IsOpen;
+            comboBox_BaudRate.Enabled = !ComDevice.IsOpen;
+            comboBox_DataBits.Enabled = !ComDevice.IsOpen;
+            comboBox_StopBits.Enabled = !ComDevice.IsOpen;
+            comboBox_CheckBits.Enabled = !ComDevice.IsOpen;
+        }
+
+        private void button_Send_Click_1(object sender, EventArgs e)
+        {
+            if (textBox_Send.Text.Length > 0)
+            {
+                textBox_Send.AppendText("\n");
+            }
+
+            //byte[] sendData = null;
+
+            //sendData = Encoding.ASCII.GetBytes(textBox_Send.Text.Trim());
+
+            //SendData(sendData);
+
+           
+
+
+            //int index = textBox_Send.
+
+            //string sendData = textBox_Send.Lines[index];
+
+            string sendData = textBox_Send.Text;
+
+            SendData(sendData);
+
+            textBox_Send.Text = "";
+        }
+    }
+}
